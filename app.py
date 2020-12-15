3dbd4d204cd8ff3b7eff797579de9da84a3eefc1
Rloui <69759725+Rloui@users.noreply.github.com>
2020-12-15 10:04:53 -0500

    add connect and close for database

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, g
+
+import models
 
 DEBUG = True
 PORT = 8000
 
 app = Flask(__name__)
+
+@app.before_request
+def before_request():
+    """Connect to the database before each request."""
+    g.db = models.DATABASE
+    g.db.connect()
+
+@app.after_request
+def after_request(response):
+    """Close the database connection after each request."""
+    g.db.close()
+    return response
+
 
 @app.route('/')
 def index():
