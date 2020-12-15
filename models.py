ad006827649eb82066bbd53d97aa8c726cb5d479
Rloui <69759725+Rloui@users.noreply.github.com>
2020-12-15 14:40:14 -0500

    added Users class to model

--- models.py
+++ models.py
@@ -1,8 +1,18 @@
 from peewee import *
+from flask_login import UserMixin
 
 import datetime
 
 DATABASE = PostgresqlDatabase('travel_app')
+
+class User(UserMixin, Model):
+    username = CharField(unique=True)
+    email = CharField(unique=True)
+    password = CharField()
+
+    class Meta:
+        database = DATABASE
+
 
 class Trip(Model):
     destination = CharField()
@@ -15,6 +25,6 @@ class Trip(Model):
 
 def initialize():
     DATABASE.connect()
-    DATABASE.create_tables([Trip], safe=True)
+    DATABASE.create_tables([User, Trip], safe=True)
     print('TABLES Created')
     DATABASE.close()
