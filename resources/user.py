1636eed2900a3d850152cdb24ac9cb37dd5c5dd5
Rloui <69759725+Rloui@users.noreply.github.com>
2020-12-15 21:00:23 -0500

    changed user to users in user.py

--- resources/user.py
+++ resources/user.py
@@ -7,17 +7,25 @@ from playhouse.shortcuts import model_to_dict
 
 user = Blueprint('users', 'user', url_prefix='/user')
 
+
+# Show Route
+# @user.route('/', methods=['GET'])
+# def get_all_users():
+#     try:
+
+
+# Register Route
 @user.route('/register', methods=['POST'])
 def register():
     payload = request.get_json()
 
     payload['email'] = payload['email'].lower()
     try:
-        models.User.get(models.User.email == payload['email'])
+        models.Users.get(models.Users.email == payload['email'])
         return jsonify(data={}, status={'code': 401, 'message': 'A user with that name already exists'})
     except models.DoesNotExist:
         payload['password'] = generate_password_hash(payload['password'])
-        user = models.User.create(**payload)
+        user = models.Users.create(**payload)
 
         #starts user session
         login_user(user)
@@ -29,3 +37,21 @@ def register():
         del user_dict['password']
 
         return jsonify(data=user_dict, status={'code': 201, 'message': 'Success'})
+
+# Log in Route
+@user.route('/login', methods=['POST'])
+def login():
+    payload = request.get_json()
+    print('payload:', payload)
+    try:
+        user = models.Users.get(models.Users.email == payload['email'])
+        user_dict = model_to_dict(user)
+        if(check_password_hash(user_dict['password'], payload['password'])):
+            del user_dict['password']
+            login_user(user)
+            print(user, 'this is user')
+            return jsonify(data=user_dict, status={'code': 200, 'message': 'Success'})
+        else:
+            return jsonify(data={}, status={'code': 401, 'message': 'Username or Password is incorrect'})
+    except models.DoesNotExist:
+        return jsonify(data={}, status={"code": 401, "message": "Username or Password is incorrect"})
