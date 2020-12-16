6cf4e140111a481b9534b9049e484976dfaa4f58
Rloui <69759725+Rloui@users.noreply.github.com>
2020-12-16 16:32:31 -0500

    fixed drop tables commands

--- seed.sql
+++ seed.sql
@@ -1,4 +1,4 @@
-DELETE FROM users
-DELETE FROM trip
-DELETE FROM trip_bridge
+DROP TABLE users;
+DROP TABLE trip;
+DROP TABLE trip_bridge;
 -- INSERT INTO trips (destination, date, budget, created_at) VALUES ('Paris, France', 2021-01-28, 500)
